71a80c4135a248ffc154ae18e4a9be117b34901b
Claude.fauconnet <claude.fauconnet@neuf.fr>
2018-02-02 16:24:47 +0100

    Initial commit

---
+++ bin/scoreSplitter..js
@@ -0,0 +1,20 @@
+
+var fs=require('fs');
+var PDFImage = require("pdf-image").PDFImage;
+
+
+
+var scoreSplitter={
+split:function(pdfFile) {
+    var pdfImage = new PDFImage("../data/rameau1.pdf");
+    pdfImage.convertPage(0).then(function (imagePath) {
+        // 0-th page (first page) of the slide.pdf is available as slide-0.png
+        fs.existsSync("../data/rameau1.png") // => true
+    });
+
+}
+
+
+
+}
+module.exports=scoreSplitter;
---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+var scoreSplitter=require( "../bin/scoreSplitter.js")
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+
+router.post('/', function(req, res, next) {
+    scoreSplitter.split("aa");
+});
+
+module.exports = router;
